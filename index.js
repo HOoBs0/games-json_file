50a8213a10b52f2a03fec88c6092ef30e8dc5f11
HOoBs0 <ehabgeorgesamy@gmail.com>
2026-01-31 19:10:16 +0200

    Initialize JSON server with routing and middleware

---
+++ index.js
@@ -0,0 +1,18 @@
+// index.js
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+// الـ Rewrite ده مهم عشان Vercel يفهم المسارات
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1'
+}));
+server.use(router);
+
+server.listen(3000, () => {
+  console.log('JSON Server is running');
+});
+
+module.exports = server;
